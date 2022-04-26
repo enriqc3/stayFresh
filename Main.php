c3732bcd2d25d4ccc931d0cbcab61e3194de85b0
enriqc3 <57701034+enriqc3@users.noreply.github.com>
2022-04-26 13:35:14 -0700

    Initial web design

---
+++ Main.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Stay Fresh</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/styles.css" />
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+	</head>
+	<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		<div class="container">
+			<a class="navbar-brand" href="#"><img src="imgs/logo5.png"/></a>
+			<button 
+				class="navbar-toggler" 
+				type="button" 
+				data-bs-toggle="collapse" 
+				data-bs-target="#navbarNav" 
+				aria-controls="navbarNav" 
+				aria-expanded="false" 
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+				<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Features</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Pricing</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled">Disabled</a>
+				</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+		<header class= "page-header gradient">
+			<div class= "container pt-3">
+				<div class="row align-items-center justify-content-center">
+					<div class="col-md-5">
+						<h2> Welcome to Stay Fresh </h2>
+						<p> We are a team of dedicated individuals 
+							who are dedicated to providing you with 
+							the best possible experience. 
+						</p>
+						<p> - Keeping it fresh since 2021 </p>
+					</div>
+						<div class="col-md-5">
+							<img 
+								src="imgs/landingImg2.jpg"
+								alt="header image"
+								style = "object-fit:scale-down;
+								width: 90%;
+								height: auto;
+								transform: rotate(90deg);
+								margin-left: 15%;"
+								/>
+						</div>
+				</div>
+			</div>
+		</header>
+		<script src= "js/bootstrap.min.js"></script>
+	</body>
+</html>
