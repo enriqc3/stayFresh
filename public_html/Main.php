1664934fa272cb84db2925712b43acafa054cd82
enriqc3 <57701034+enriqc3@users.noreply.github.com>
2022-05-03 21:52:48 -0700

    Add files via upload

---
+++ public_html/Main.php
@@ -0,0 +1,5 @@
+<?php require_once('../stayFresh/db.php'); ?>
+<?php require('../stayFresh/components/head.inc.php'); ?>
+<?php include('../stayFresh/components/navbar.inc.php'); ?>
+<?php include('../stayFresh/components/header.inc.php'); ?>
+<?php include('../stayFresh/components/footer.inc.php'); ?>
