f974674a4f54ad77464bb00f57b6c55cb8614a01
enriqc3 <57701034+enriqc3@users.noreply.github.com>
2022-05-06 05:25:43 -0700

    Delete public_html directory

--- public_html/Main.php
+++
@@ -1,5 +0,0 @@
-<?php require_once('../stayFresh/db.php'); ?>
-<?php require('../stayFresh/components/head.inc.php'); ?>
-<?php include('../stayFresh/components/navbar.inc.php'); ?>
-<?php include('../stayFresh/components/header.inc.php'); ?>
-<?php include('../stayFresh/components/footer.inc.php'); ?>
