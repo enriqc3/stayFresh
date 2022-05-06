1550fba2aed3a9d5264e80c2f575f578d1354865
enriqc3 <57701034+enriqc3@users.noreply.github.com>
2022-05-06 05:25:53 -0700

    Delete stayFresh directory

--- stayFresh/components/footer.inc.php
+++
@@ -1,7 +0,0 @@
-		<footer class ="gradient">
-			<div class="container-fluid text-center"></div>
-			<span>Stay Fresh 2020</span>
-		</footer>
-		<script src= "js/bootstrap.min.js"></script>
-	</body>
-</html>
